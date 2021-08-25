d4e36499919564a75370b134f4193b6661602a00
vgoylo <y_draiv.by@mail.ru>
2021-08-25 11:07:25 +0300

    Create telegram bot

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -1,1 +1,14 @@
-require File.expand_path('../config/enviroment', __dir__)
+require File.expand_path('../config/environment', __dir__)
+
+require 'telegram/bot'
+
+token = '1960253759:AAEaqU6RavaQ0kHMqopHSn5DhrV1oEaR2Bc'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    if message.text.include?('Hello')
+      bot.api.send_message(chat_id: message.chat.id, text: "123456789")
+    end
+  end
+end
+
