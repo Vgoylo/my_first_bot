f65d18603073946149c1686aca59a7c80d5c1397
vgoylo <y_draiv.by@mail.ru>
2021-08-26 10:34:35 +0300

    Setup postgres && add user & bot models

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -6,9 +6,7 @@ token = '1960253759:AAEaqU6RavaQ0kHMqopHSn5DhrV1oEaR2Bc'
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    if message.text.include?('Hello')
-      bot.api.send_message(chat_id: message.chat.id, text: "123456789")
-    end
+
   end
 end
 
