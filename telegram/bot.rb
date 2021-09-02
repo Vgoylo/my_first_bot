a7440be6a9b214eea54acfa800ba73f665f1f8f9
vgoylo <y_draiv.by@mail.ru>
2021-09-02 10:58:34 +0300

    Add url for bad

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -14,7 +14,8 @@ Telegram::Bot::Client.run(token) do |bot|
     when'good'
       bot.api.send_message(chat_id: message.chat.id, text: '☺️')
     when 'bad' 
-      bot.api.send_message(chat_id: message.chat.id, text: 'Ok')
+      url = 'https://www.cnet.com/a/img/-qQkzFVyOPEoBRS7K5kKS0GFDvk=/940x0/2020/04/16/7d6d8ed2-e10c-4f91-b2dd-74fae951c6d8/bazaart-edit-app.jpg'
+      bot.api.send_photo(chat_id: message.chat.id, photo: url, caption: 'test')
     end
   end
 end
