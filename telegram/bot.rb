ae0be6e0f0650f7a0c76ae76e885210b9d61fc9c
vgoylo <y_draiv.by@mail.ru>
2021-09-07 09:04:27 +0300

    Create bot which reminds

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -8,18 +8,24 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
  
     input = message.text 
-    case input 
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: 'Hi how are you?') 
-    when'good'
-      bot.api.send_message(chat_id: message.chat.id, text: '☺️')
-    when 'bad' 
+    if input.include?('Remind me')
+      input.gsub!('Remind me ', '')
+      sleep 20
+      bot.api.send_message(chat_id: message.chat.id, text: input) 
 
-      url = "https://loremflickr.com/320/240/#{SecureRandom.random_number(10000)}"
-      
-      bot.api.send_photo(chat_id: message.chat.id, photo: url, caption: 'test')
-     
     end
+    # case input 
+    # when '/start'
+    #   bot.api.send_message(chat_id: message.chat.id, text: 'Hi how are you?') 
+    # when'good'
+    #   bot.api.send_message(chat_id: message.chat.id, text: '☺️')
+    # when 'bad' 
+
+    #   url = "https://loremflickr.com/320/240/#{SecureRandom.random_number(10000)}"
+      
+    #   bot.api.send_photo(chat_id: message.chat.id, photo: url, caption: 'test')
+     
+    # end
   end
 end
  
